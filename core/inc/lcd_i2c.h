7c2213fac52ebcdc7f46d8cb7759ef934b7bbce8
Sanica-19 <sanicasanu192004@gmail.com>
2026-03-20 17:23:38 +0530

    Added lcd_i2c.h

---
+++ core/inc/lcd_i2c.h
@@ -0,0 +1,13 @@
+#ifndef __LCD_I2C_H__
+#define __LCD_I2C_H__
+
+#include "main.h"
+
+void lcd_init(void);
+void lcd_send_cmd(char cmd);
+void lcd_send_data(char data);
+void lcd_send_string(char *str);
+void lcd_clear(void);
+void lcd_set_cursor(int row, int col);
+
+#endif
